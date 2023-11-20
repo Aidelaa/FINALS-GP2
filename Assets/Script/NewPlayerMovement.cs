ce9191fcc2276eafc365fe09272dd0c0e1a9bec6
ronssae <miguel.malsi@ciit.edu.ph>
2023-11-20 18:14:45 +0800

    Movement, Destroy Kalat, Pinagtabi tabi mga Rooms

---
+++ Assets/Script/NewPlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed;
+    public int KalatCount;
+    public Rigidbody2D rb;
+    public Animator anim;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * MoveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Kalat"))
+        {
+            KalatCount++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
